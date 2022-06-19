6ee1cfb51a62f162690b36270e687440f83f981c
Friggo <Diogo.Friggo@dnv.com>
2022-06-19 12:24:47 -0300

    swapped ui

--- app.py
+++ app.py
@@ -1,42 +1,21 @@
-from dash import Dash, callback, html, dcc
+from dash import Dash
 import dash_bootstrap_components as dbc
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
-from whitenoise import WhiteNoise   #for serving static files on Heroku
+import gunicorn # whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
+from whitenoise import WhiteNoise # for serving static files on Heroku
 
-# Instantiate dash app
-app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+from vicinity.ui import layout
 
-# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
-server = app.server 
+# AVAILABLE THEMES:
+# CERULEAN , COSMO , CYBORG , DARKLY , FLATLY , JOURNAL , LITERA , LUMEN ,
+# LUX , MATERIA , MINTY , PULSE , SANDSTONE , SIMPLEX , SKETCHY , SLATE ,
+# SOLAR , SPACELAB , SUPERHERO , UNITED , YETI
 
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = layout
+app.title = 'Vicinity'
 
-# Define Dash layout
-def create_dash_layout(app):
+server = app.server
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
 
-    # Set browser tab title
-    app.title = "Your app title" 
-    
-    # Header
-    header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
-    
-    # Body 
-    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """), html.Br(), html.Img(src='charlie.png')])
-
-    # Footer
-    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)""")])
-    
-    # Assemble dash layout 
-    app.layout = html.Div([header, body, footer])
-
-    return app
-
-# Construct the dash layout
-create_dash_layout(app)
-
-# Run flask app
-if __name__ == "__main__": app.run_server(debug=False, host='0.0.0.0', port=8050)
+if __name__ == '__main__':
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
---
+++ vicinity/ui.py
@@ -0,0 +1,41 @@
+from dash import dcc
+import dash_bootstrap_components as dbc
+
+# from vicinity.core import get_source_data_path
+
+layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Label('File path', html_for='file_path'),
+            dbc.Input(
+                id='file_path',
+                persistence=True),
+                # value=get_source_data_path()),
+        ], width=12),
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Label('Columns to plot', html_for='columns'),
+            dcc.Dropdown(id='columns', multi=True),
+        ], width=12),
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Label('X column (blank will default to df.index)', html_for='x_col'),
+            dcc.Dropdown(id='x_col', persistence=True),
+        ], width=4),
+        dbc.Col([
+            dbc.Label('Row start', html_for='row_start'),
+            dbc.Input(id='row_start', persistence=True),
+        ], width=1),
+        dbc.Col([
+            dbc.Label('Row end', html_for='row_end'),
+            dbc.Input(id='row_end', persistence=True),
+        ], width=1),
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='output', style={'margin-top': '3px'})
+        ], width=12),
+    ]),
+], fluid=True)
