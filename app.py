717b1c16c3111223d9801ac7570d25c35245d7c3
Friggo <Diogo.Friggo@dnv.com>
2022-06-19 13:06:44 -0300

    All callbacks must be defined before the server starts

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from dash import Dash
 import gunicorn # whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
+from whitenoise import WhiteNoise # for serving static files on Heroku
 
-from vicinity.app import app, server
 from vicinity.ui import layout
 from vicinity.logic import *
+from vicinity.app import app
+
+# All callbacks must be defined before the server starts
 
 app.layout = layout
+app.title = 'Vicinity'
+
+server = app.server
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
--- vicinity/app.py
+++ vicinity/app.py
@@ -1,6 +1,5 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
-from whitenoise import WhiteNoise # for serving static files on Heroku
 
 # AVAILABLE THEMES:
 # CERULEAN , COSMO , CYBORG , DARKLY , FLATLY , JOURNAL , LITERA , LUMEN ,
@@ -8,7 +7,3 @@ from whitenoise import WhiteNoise # for serving static files on Heroku
 # SOLAR , SPACELAB , SUPERHERO , UNITED , YETI
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = 'Vicinity'
-
-server = app.server
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
